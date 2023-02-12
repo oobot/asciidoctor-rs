a0f7d28d8034e16a57220322cd65b1938cafa2f5
oobot <bitweng@gmail.com>
2023-02-12 11:13:30 +0800

    reader

--- src/reader.rs
+++ src/reader.rs
@@ -5,31 +5,36 @@ use async_std::fs;
 use async_std::path::Path;
 use once_cell::sync::Lazy;
 
+pub static BLOCK_TOKENS: Lazy<HashSet<&str>> = Lazy::new(|| HashSet::from(
+    ["|===",  "====", "----", "____", "****", "++++"]
+));
+
 pub async fn read_file(file: &Path) -> Result<LinkedList<String>, Error> {
     Ok(read(&fs::read_to_string(file).await?))
 }
 
 pub fn read(text: &str) -> LinkedList<String> {
-    // let lines: Vec<&str> = text.split("\n").collect();
     let mut list = LinkedList::new();
-
     let mut block = String::new();
     let mut combine_block = false;
     for line in text.split("\n\n") {
         block.push_str(line);
         block.push_str("\n\n");
 
-        if !combine_block && has_block_token(line) {
+        if !combine_block && only_one_token(line) {
             combine_block = true;
             continue;
         }
 
-        if combine_block && is_token_end(line) {
+        if combine_block && only_one_token(line) && is_token_end(line) {
             combine_block = false;
         }
 
         if !combine_block {
-            list.push_back(block);
+            match list.is_empty() { // header
+                true => list.append(&mut split_header(&block)),
+                _ => list.push_back(block.trim().to_owned()),
+            }
             block = String::new();
         }
     }
@@ -38,24 +43,31 @@ pub fn read(text: &str) -> LinkedList<String> {
     list
 }
 
-fn has_block_token(line: &str) -> bool {
-    for l in line.split("\n") {
-        if BLOCK_TOKENS.contains(l) {
-            return true;
+fn split_header(header: &str) -> LinkedList<String> {
+    header.split("\n").filter_map(|v|
+        match v.trim().is_empty() {
+            true => None,
+            _ => Some(v.trim().to_owned())
         }
-    }
-    false
+    ).collect()
 }
 
-fn is_token_end(line: &str) -> bool {
+fn only_one_token(block: &str) -> bool {
+    let mut count = 0;
+    for b in block.split("\n") {
+        count += match BLOCK_TOKENS.contains(b.trim()) {
+            true => 1,
+            _ => 0
+        };
+    }
+    count == 1
+}
+
+fn is_token_end(block: &str) -> bool {
     for token in BLOCK_TOKENS.iter() {
-        if line.ends_with(token) {
+        if block.ends_with(token) {
             return true;
         }
     }
     false
 }
-
-static BLOCK_TOKENS: Lazy<HashSet<&str>> = Lazy::new(|| HashSet::from(
-    ["|===",  "====", "----", "____", "****", "++++"]
-));
